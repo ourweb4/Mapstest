7a3dc48c13867eeda9942158837bd54bb56b12f0
Bill <office@ourweb.net>
2018-06-20 10:04:31 -0400

    fix

--- Mapstest/Mapstest.Android/MainActivity.cs
+++ Mapstest/Mapstest.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace Mapstest.Droid
 {
@@ -23,6 +24,11 @@ namespace Mapstest.Droid
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
